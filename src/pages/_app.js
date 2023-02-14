12181cc007008b013b40a88c2f41b683460e7957
Alex España <julii1295@gmail.com>
2023-02-13 19:59:16 -0500

    Ignorando multimedia

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/_colors.module.scss'
 import '../styles/_heredar.module.scss'
 import '../styles/Profile.module.scss'
-import 'tailwindcss/tailwind.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
