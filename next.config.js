20d4e73e562d6a3e6b3ab83c3cf76c5e31e97fe9
Alex España <julii1295@gmail.com>
2023-02-13 21:24:42 -0500

    Cambiando config of next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})  
+
+module.exports = withPWA( {        
+    reactStrictMode: true,    
+  pwa: {
+      name: 'To show like app native',
+      manifestPath: './public/manifest.json',
+      dest: 'public',
+      register: true,
+      mode: 'production',
+      skipWaiting: true,
+      // disable: false,
+      disable: process.env.NODE_ENV === 'development',
+  },
+} )
+
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
