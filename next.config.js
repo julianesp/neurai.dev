6bc4c1214354219ba620b93242078b5babf4ff1a
Alex España <julii1295@gmail.com>
2023-02-13 21:28:12 -0500

    Quitando config de next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})  
-
-module.exports = withPWA( {        
-    reactStrictMode: true,    
-  pwa: {
-      name: 'To show like app native',
-      manifestPath: './public/manifest.json',
-      dest: 'public',
-      register: true,
-      mode: 'production',
-      skipWaiting: true,
-      // disable: false,
-      disable: process.env.NODE_ENV === 'development',
-  },
-} )
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    appDir: false,
   },
 }
 
