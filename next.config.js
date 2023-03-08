7140ecb195552845e4277c51642e231ad4cc42ca
Julián España <julii1295@gmail.com>
2023-03-08 11:49:11 -0500

    ADDing styles to the footer and changing color to the background

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path');
+
 const nextConfig = {
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
 
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,52 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
-import dev from '../assets/img/presentacion/park.jpg'
-import sena1 from '../assets/img/logos/sena.png'
-import platzi from "../assets/img/logos/platzi.jpg"
-import park from '../assets/img/presentacion/park.jpg'
+import linkedin from '/public/network/linkedin.png'
+import github from '/public/network/github_2.png'
+import twitter from '/public/network/twitter.svg'
+import facebook from '/public/network/facebook.png'
 import styles from '../styles/Footer.module.scss'
 
 const Footer = () => {
+
     return (
-        <div className={styles.footer}>
 
-            {/* <Image className={styles['footer-img']} src={park} alt="Parque Colón Putumayo" /> */}
-
-            <section className={styles.info}>
-                <div className={styles.education}>
-                    <h4>Desarrollado por:<br />Julián España</h4>
-                    <h5>Técnico en sistemas </h5>
+        <section className={styles.footer}>
+            <h3>Contáctame</h3>
+            <section className={styles.contactame}>
+                <article className={styles.contactame__red}>
                     <Image
-                        src={dev}
-                        alt="Logo SENA"
-                        width={50}
-                        height={50}
+                        className={styles.imagen}
+                        alt='LinkedIn'
+                        src={linkedin}
                     />
-                    <h5>Tecnólogo en Análisis de sistemas</h5>
+                </article>
+                <article>
                     <Image
-                        src={sena1}
-                        alt="Logo SENA"
-                        width={50}
-                        height={50}
+                    className={styles.imagen}
+                        alt='GitHub'
+                        src={github}
                     />
-                    <h5>Desarrollador Front-End</h5>
+                </article>
+                <article>
                     <Image
-                        src={platzi}
-                        alt="Logo Platzi"
-                        width={50}
-                        height={50}
+                        alt='Twitter'
+                        src={twitter}
                     />
-                    <h5>Parque</h5>
+                </article>
+                <article>
                     <Image
-                        src={park}
-                        alt="Logo Platzi"
-                        width={50}
-                        height={50}
+                        alt='Facebook'
+                        src={facebook}
                     />
-                </div>
+                </article>
             </section>
-        </div>
-
+        </section>
     )
 }
 
