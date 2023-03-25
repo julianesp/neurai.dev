f38fe3059b84100a780c14bf0868782f783f877e
Julián España <julii1295@gmail.com>
2023-03-24 23:19:33 -0500

    Deleting configuraciones de next.config.js v.2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-const path = require('path')
-
 const nextConfig = {
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
 
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+
 }
 
 module.exports = nextConfig
