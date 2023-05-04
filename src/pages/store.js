aa2e9aa52a0a5deac4dcf638896c21a17e16395d
Julián España <julii1295@gmail.com>
2023-05-03 22:18:59 -0500

    Starting to add config to mongodb

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
+// import Productos from '../databases/data.json'
 import styles from '../styles/Store.module.scss'
+import Image from 'next/image'
 
-const store = () => {
+const store = ({ entradas }) => {
+
+  const productos = require('../databases/data.json')
+
+  const [useProductos, useSetProductos] = useState([])
+
+  const showAcce = async () => {
+    const url = productos
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json
+
+    console.log(resultado)
+  }
+
+  showAcce()
+  // useEffect(() => {
+  // }, [])
+
+  console.log(entradas)
+
   return (
     <Layout>
-        <section className={styles.container}>
-            <h1>tienda</h1>
+      <section className={styles.container}>
+        <h1>tienda</h1>
 
-        </section>
+
+
+      </section>
     </Layout>
   )
 }
+
+// export async function getServerSideProps() {
+//   const url = productos
+//   const respuesta = await fetch(url)
+//   const entradas = await respuesta.json
+
+
+
+//   return {
+//     props: {
+//       entradas
+//     }
+//   }
+// }
 
 export default store
