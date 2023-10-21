9681a730f42bd9b2a60619a1972203e13feff3a4
Alex España <julii1295@gmail.com>
2023-10-21 18:23:03 -0500

    Modificando enlaces del navbar || Changind to grid to me presentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Profile from './profile'
-// import App from '../app.js'
 
 const Index = () => {
   return (
     <Layout>
-      <Profile/>
-      {/* <App/> */}
+      <Profile />
     </Layout>
   )
 }
